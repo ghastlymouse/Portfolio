86a0800ce5ad5823d2677dc59a279b22871cf8e4
Ghastly Mouse <gusdnr0839@gmail.com>
2025-02-10 14:38:11 +0900

    add: 기술 스택 카드 추가

---
+++ src/app/about/_components/SkillCard.tsx
@@ -0,0 +1,38 @@
+type SkillCardType = {
+  title: string;
+  titleIcon: React.ReactNode;
+  skills: string[];
+  colors: string[];
+  icons: React.ReactNode[];
+};
+
+const SkillCard = (skillInfo: SkillCardType) => {
+  const { title, titleIcon, skills, colors, icons } = skillInfo;
+
+  return (
+    <div className="bg-white bg-opacity-60 border-2 rounded-lg shadow-custom w-96 p-4">
+      <div className="flex items-start gap-1 pb-8">
+        {titleIcon}
+        <span className="text-3xl">{title}</span>
+      </div>
+      <div className="mb-8">
+        {skills.map((skill, idx) => (
+          <div key={skill} className="mb-8">
+            <div
+              className="flex items-center gap-2 text-2xl"
+              style={{
+                color: colors[idx],
+              }}
+            >
+              {icons[idx]}
+              {skill}
+            </div>
+            <div className="w-full h-[1px] bg-gray-500 shadow-custom shadow-gray-100"></div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SkillCard;
---
+++ src/app/about/_components/Skills.tsx
@@ -0,0 +1,23 @@
+import SkillCard from "./SkillCard";
+import { FRONTEND_SKILLS } from "@/constants/skills/frontendSkills";
+import { STYLING_SKILLS } from "@/constants/skills/stylingSkills";
+import { STATE_MANAGEMENT_SKILLS } from "@/constants/skills/stateManagementSkills";
+import { BACKEND_SERVICES_SKILLS } from "@/constants/skills/backendServicesSkills";
+
+const Skills = () => {
+  return (
+    <section>
+      <p className="flex justify-center text-5xl font-semibold pt-20 pb-10">
+        My Skills
+      </p>
+      <div className="grid grid-cols-4 gap-x-10">
+        <SkillCard {...FRONTEND_SKILLS} />
+        <SkillCard {...STYLING_SKILLS} />
+        <SkillCard {...STATE_MANAGEMENT_SKILLS} />
+        <SkillCard {...BACKEND_SERVICES_SKILLS} />
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
