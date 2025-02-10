35297c1c5ab5d572c182a3baaf3edf87ef2b4f88
Ghastly Mouse <gusdnr0839@gmail.com>
2025-02-10 14:37:15 +0900

    add: 첫 페이지 내용 추가

---
+++ src/app/_components/DevTimeline.tsx
@@ -0,0 +1,20 @@
+const DevTimeline = () => {
+  return (
+    <section className="flex flex-col items-center gap-10">
+      <div className="rounded-full border-8 border-sky-500 w-64 h-64 flex justify-center items-center">
+        d
+      </div>
+      <div className="rounded-full border-8 border-sky-500 w-40 h-40 flex justify-center items-center">
+        d
+      </div>
+      <div className="rounded-full border-8 border-sky-500 w-72 h-72 flex justify-center items-center">
+        d
+      </div>
+      <div className="rounded-full border-8 border-sky-500 w-96 h-96 flex justify-center items-center">
+        d
+      </div>
+    </section>
+  );
+};
+
+export default DevTimeline;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
+import DevTimeline from "./_components/DevTimeline";
+import LinkCard from "./_components/LinkCard";
+
 export default function Home() {
   return (
-    <main className="w-full min-h-screen max-h-screen flex flex-col justify-center items-center">
-      안녕하세요
+    <main className="w-full min-h-screen p-12 grid grid-cols-[minmax(820px,_1fr)_minmax(0,_400px)] mb-40">
+      <section className="flex flex-col items-end gap-10 pr-20">
+        <div className="flex flex-col items-end">
+          <span className="text-[200px]">{"무시무시한 쥐's"}</span>
+          <span className="text-9xl">포트폴리오</span>
+        </div>
+        <div className="text-4xl flex flex-col items-end">
+          <p>반갑습니다</p>
+          <p>
+            프론트엔드 개발자 지망생
+            <span className="text-violet-500 text-5xl">
+              정현욱 무시무시한 쥐
+            </span>
+            입니다
+          </p>
+          <p>저를 소개하는 포트폴리오 사이트에 오신 것을 환영합니다!</p>
+        </div>
+        <LinkCard />
+      </section>
+      <DevTimeline />
     </main>
   );
 }
