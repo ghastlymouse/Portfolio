1d1daf63cad403f3d4539348258f62dcab7e396d
Ghastly Mouse <gusdnr0839@gmail.com>
2025-02-11 23:34:14 +0900

    update: 사이드바 기능 추가 사이드바의 토글 버튼과 메뉴들이 스크롤을 해도 위치 고정됨, 사이드바 오픈시 스크롤 제한, 페이지 이동시 사이드바 닫기

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -4,11 +4,25 @@ import { useAppDispatch, useAppSelector } from "@/hooks/reduxHooks";
 import { toggleNavVisible } from "@/redux/navSlice";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { useEffect } from "react";
 import { LiaGripLinesVerticalSolid } from "react-icons/lia";
 
 const Sidebar = () => {
   const isOpened = useAppSelector((state) => state.nav.isNavVisible);
   const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (isOpened) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+
+    return () => {
+      document.body.style.overflow = "";
+      document.body.style.position = "";
+    };
+  }, [isOpened]);
 
   return (
     <div className="relative">
@@ -22,32 +36,54 @@ const Sidebar = () => {
 
       {/* 사이드바 애니메이션 */}
       <motion.nav
-        initial={{ x: "-90%" }}
-        animate={{ x: isOpened ? 0 : "-90%" }}
+        initial={{ x: "-80%" }}
+        animate={{ x: isOpened ? 0 : "-80%" }}
         transition={{ type: "spring", stiffness: 1200, damping: 150 }}
         className="fixed top-0 left-0 w-64 bg-black text-white z-40 h-full pr-4"
       >
-        {/* 사이드바 닫기 버튼 */}
-        <button
-          className="absolute top-1/2 right-0 z-30 bg-black text-white rounded-full"
-          onClick={() => {
-            if (isOpened) {
-              dispatch(toggleNavVisible(false));
-            } else {
-              dispatch(toggleNavVisible(true));
-            }
-          }}
-        >
-          <LiaGripLinesVerticalSolid size={40} />
-        </button>
-        <Link href={"/"} className="block text-center mt-8">
-          {"HyunUk's Portfolio"}
-        </Link>
-        <ul className="flex flex-col items-center justify-center gap-10 mt-10">
-          <Link href={"/about"}>About me</Link>
-          <Link href={"/projects"}>Projects</Link>
-          <Link href={"/contact"}>Contact</Link>
-        </ul>
+        <div className="sticky top-1/2 transform -translate-y-1/2">
+          {/* 사이드바 닫기 버튼 */}
+          <button
+            className="absolute top-1/2 right-0 z-30 bg-black text-white rounded-full"
+            onClick={() => {
+              if (isOpened) {
+                dispatch(toggleNavVisible(false));
+              } else {
+                dispatch(toggleNavVisible(true));
+              }
+            }}
+          >
+            <LiaGripLinesVerticalSolid size={40} />
+          </button>
+          <Link
+            href={"/"}
+            onClick={() => dispatch(toggleNavVisible(false))}
+            className="block text-center mt-8"
+          >
+            <p>{"HyunUk's"}</p>
+            <p>Portfolio</p>
+          </Link>
+          <ul className="flex flex-col items-center justify-center gap-10 mt-10">
+            <Link
+              href={"/about"}
+              onClick={() => dispatch(toggleNavVisible(false))}
+            >
+              About me
+            </Link>
+            <Link
+              href={"/projects"}
+              onClick={() => dispatch(toggleNavVisible(false))}
+            >
+              Projects
+            </Link>
+            <Link
+              href={"/contact"}
+              onClick={() => dispatch(toggleNavVisible(false))}
+            >
+              Contact
+            </Link>
+          </ul>
+        </div>
       </motion.nav>
     </div>
   );
