4ac017fed7847fe7cd1d8443c3e8936e8820f7d2
Ghastly Mouse <gusdnr0839@gmail.com>
2025-02-14 21:18:12 +0900

    type: 타입 추가

---
+++ src/app/type/projectInfoType.ts
@@ -0,0 +1,10 @@
+export type ProjectInfoType = {
+  name: string;
+  member: number;
+  duration: string;
+  image: string;
+  description: string;
+  features: string[];
+  techs: string[];
+  source: string;
+};
