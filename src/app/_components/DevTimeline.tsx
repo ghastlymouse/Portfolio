0d50c14698efa7a62948a152d106930be277698f
Ghastly Mouse <gusdnr0839@gmail.com>
2025-02-15 22:45:15 +0900

    add: devTimeline 관련 추가

--- src/app/_components/DevTimeline.tsx
+++ src/app/_components/DevTimeline.tsx
@@ -1,18 +1,18 @@
+import {
+  firstCircle,
+  fourthCircle,
+  secondCircle,
+  thirdCircle,
+} from "@/constants/timelineCircle";
+import TimelineCircle from "./TimelineCircle";
+
 const DevTimeline = () => {
   return (
     <section className="flex flex-col items-center gap-10">
-      <div className="rounded-full border-8 border-sky-500 w-64 h-64 flex justify-center items-center shadow-custom hover:shadow-hover hover:scale-110 transition-transform duration-300 ease-in-out">
-        d
-      </div>
-      <div className="rounded-full border-8 border-sky-500 w-40 h-40 flex justify-center items-center shadow-custom hover:shadow-hover hover:scale-110 transition-transform duration-300 ease-in-out">
-        d
-      </div>
-      <div className="rounded-full border-8 border-sky-500 w-72 h-72 flex justify-center items-center shadow-custom hover:shadow-hover hover:scale-110 transition-transform duration-300 ease-in-out">
-        d
-      </div>
-      <div className="rounded-full border-8 border-sky-500 w-96 h-96 flex justify-center items-center shadow-custom hover:shadow-hover hover:scale-110 transition-transform duration-300 ease-in-out">
-        d
-      </div>
+      <TimelineCircle {...firstCircle} />
+      <TimelineCircle {...secondCircle} />
+      <TimelineCircle {...thirdCircle} />
+      <TimelineCircle {...fourthCircle} />
     </section>
   );
 };
